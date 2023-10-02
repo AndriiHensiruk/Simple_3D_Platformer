00dd07ec0396a2f619c80d90142246ccdcde61f0
AndriiHensiruk <Hensiruk@gmail.com>
2023-10-02 11:24:16 +0300

    Added winpanel and hud

---
+++ Source/Simple_3D_Platformer/Public/PlatformerHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "PlatformerHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLE_3D_PLATFORMER_API APlatformerHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
